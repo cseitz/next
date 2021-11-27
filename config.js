50f9cf6d4e679e895c33f32dcac78c61a2251218
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-11-26 22:52:39 -0500

    fixed segfault

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {};
-try {
-    Object.assign(module.exports, require('esm')(module)(process.cwd() + '/next.config.mjs'));
-} catch(e) {
+// try {
+//     Object.assign(module.exports, require('esm')(module)(process.cwd() + '/next.config.mjs'));
+// } catch(e) {
 
-}
+// }
 try {
     Object.assign(module.exports, require(process.cwd() + '/next.config.js'));
 } catch(e) {
